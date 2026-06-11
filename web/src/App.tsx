bd14a98f3cb903627f54d5a1c1af7840c8e61336
Kevin Mun <munkevin@gmail.com>
2026-06-11 14:56:28 +0800

    fix: add missing Link import for footer

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
 import { lazy, Suspense } from 'react'
 import Header from './components/Header'
 import Loading from './components/Loading'
